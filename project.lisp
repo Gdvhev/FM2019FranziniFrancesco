b26d94689e1b6d1e56ee983939d4e5d0a2b67045
gdvhev <francesco2.franzini@mail.polimi.it>
2019-05-24 09:27:34 +0200

    Reworked robot controller, added operator state

--- project.lisp
+++ project.lisp
@@ -76,10 +76,13 @@
             (-> (&& (-P- robotToL3) (next (-P- robotIn 3)))  (next (-P- robotWorking))) ;If next time I reach 3, start working
             (-> (&& (-P- robotToL3) (next(!! (-P- robotIn 3)))) (next (-P- robotToL3))) ;If next time I don't reach 3, keep going
             
-            (-> (&& (-P- robotWorking) (next (-P- robotIn 3)) (-P- robotIn 3))  (next (-P- robotWorking))) ;If I am working in 3 and I don't move, keep working
-            (-> (&& (-P- robotWorking) (next (!!(-P- robotIn 3))) (-P- robotIn 3))  (next (-P- robotToL9))) ;If I am working in 3 and I  move, go to 9
-            (-> (&& (-P- robotWorking) (next (!!(-P- robotIn 9))) (-P- robotIn 9))  (next (-P- robotToL3))) ;If I am working in 9 and I  move, go to 3
-            (-> (&& (-P- robotWorking) (next (-P- robotIn 9)) (-P- robotIn 9))  (next (-P- robotWorking))) ;If I am working in 9 and I don't move, keep working
+            (-> (&& (-P- robotWorking) (next (-P- robotWorking)) (-P- robotIn 3))  (next (-P- robotIn 3))) ; If I am working in 3 and will work next time, stay in 3
+            (-> (&& (-P- robotWorking) (next (-P- robotToL9)) (-P- robotIn 3))  (next (!!(-P- robotIn 3)))); If I am working in 3 and change state, next time go away from 3
+            (-> (&& (-P- robotWorking) (-P- robotIn 3))  (!! (next (-P- robotToL3)))) ; If I am working in 3, I can only keep working or go to robotToL9 state
+            
+            (-> (&& (-P- robotWorking) (next (-P- robotWorking)) (-P- robotIn 9))  (next (-P- robotIn 9))) ; If I am working in 9 and will work next time, stay in 9
+            (-> (&& (-P- robotWorking) (next (-P- robotToL3)) (-P- robotIn 9))  (next (!!(-P- robotIn 9)))); If I am working in 9 and change state, next time go away from 9
+            (-> (&& (-P- robotWorking) (-P- robotIn 9))  (!! (next (-P- robotToL9)))) ; If I am working in 9, I can only keep working or go to robotToL3 state
             
 ))) 
 
@@ -139,6 +142,29 @@
     )
 )
 
+(defconstant operatorState ;10 8
+    (alw (&&
+            (!! (&& (-P- operatorToL10) (-P- operatorToL8)))
+            (!! (&& (-P- operatorWorking) (-P- operatorToL10)))
+            (!! (&& (-P- operatorWorking) (-P- operatorToL8)))
+            (|| (-P- operatorWorking) (-P- operatorToL10) (-P- operatorToL8)) ;exactly one of those proposition is true at each time
+            
+            (-> (&& (-P- operatorToL10) (next (-P- operatorIn 10)))  (next (-P- operatorWorking))) ;If next time I reach 10, start working
+            (-> (&& (-P- operatorToL10) (next(!! (-P- operatorIn 10)))) (next (-P- operatorToL10))) ;If next time I don't reach 10, keep going
+            
+            (-> (&& (-P- operatorToL8) (next (-P- operatorIn 8)))  (next (-P- operatorWorking))) ;If next time I reach 8, start working
+            (-> (&& (-P- operatorToL8) (next(!! (-P- operatorIn 8)))) (next (-P- operatorToL8))) ;If next time I don't reach 8, keep going
+            
+            (-> (&& (-P- operatorWorking) (next (-P- operatorWorking)) (-P- operatorIn 8))  (next (-P- operatorIn 8))) ; If I am working in 8 and will work next time, stay in 8
+            (-> (&& (-P- operatorWorking) (next (-P- operatorToL10)) (-P- operatorIn 8))  (next (!!(-P- operatorIn 8)))); If I am working in 8 and change state, next time go away from 8
+            (-> (&& (-P- operatorWorking) (-P- operatorIn 8))  (!! (next (-P- operatorToL8)))) ; If I am working in 8, I can only keep working or go to operatorToL10 state
+            
+            (-> (&& (-P- operatorWorking) (next (-P- operatorWorking)) (-P- operatorIn 10))  (next (-P- operatorIn 10))) ; If I am working in 10 and will work next time, stay in 10
+            (-> (&& (-P- operatorWorking) (next (-P- operatorToL8)) (-P- operatorIn 10))  (next (!!(-P- operatorIn 10)))); If I am working in 10 and change state, next time go away from 10
+            (-> (&& (-P- operatorWorking) (-P- operatorIn 10))  (!! (next (-P- operatorToL10)))) ; If I am working in 10, I can only keep working or go to operatorToL8 state
+            
+))) 
+
 
 ;Constraints imposed by location
 (defconstant forbiddenPlaces
@@ -156,6 +182,7 @@
             (-P- robotIn 3)
             (-P- operatorIn 10)
             (-P- robotWorking)
+            (-P- operatorWorking)
             
             ;debug
             ;(-P- robotIn 5)
@@ -163,6 +190,7 @@
             ;(-P- robotToL9)
             ;(next(-P- robotIn 10))
             ;(next(-P- operatorIn 10))
+            (next (next (-P- robotIn 3)))
             ))
     
 ;Objective property
@@ -176,8 +204,19 @@
     ;(somF  (-P- collision) ) ;Simple collision
     (somF (|| (&& (-P- collision) (-P- robotMoving)) (&& (next (-P- collision)) (-P- robotMoving))  )) ;Moving collision and converging to operator position 
 )
-    
-(ae2sbvzot:zot 30
+
+(defconstant niceSimulation
+    (&&
+        (next (!! (-P- robotIn 3))) ;Next time instant go to 9
+        (next (somF (&& (-P- robotWorking) (-P- robotIn 3)))) ;Counting from next time instant, robot does at least a full loop
+        
+        (next (!! (-P- operatorIn 10))) ;Next time instant go to 8
+        (next (somF (&& (-P- operatorWorking) (-P- operatorIn 10)))) ;Counting from next time instant, operator does at least a full loop
+
+))
+
+
+(ae2sbvzot:zot 20
         (yesterday(&&
                     
                     init
@@ -194,9 +233,13 @@
                     operatorPredicates
                     realisticOperatorMovement
                     operatorAlwaysSomewhere
+                    operatorState
                     
                     ;Collision definition
                     collision
                     collisionEnforcer
+                    
+                    ;Simulation guiding
+                    niceSimulation
                 ))
     )
