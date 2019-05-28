05fc11e9acac16e3f70192a9098d8a38067b9c18
gdvhev <francesco2.franzini@mail.polimi.it>
2019-05-28 09:46:03 +0200

    Comment refactoring

--- project.lisp
+++ project.lisp
@@ -5,9 +5,9 @@
 
 (defvar *ncells* (loop for x from 1 to 12 collect x));;{1..12}
 
-(defun adjacent (n1 n2)
+(defun adjacent (n1 n2) ;Returns true iff n1 and n2 are the indexes of two adjacent cells in the grid
    (||
-        (= n1 n2)
+        (= n1 n2) ;Each cell is adjacent to itself
         
         (&& (&& (/= n1 4) (/= n1 8) (/= n1 12)) (= n2 (+ n1 1))) ; if not on right border, one right
         (&& (&& (/= n1 1) (/= n1 5) (/= n1 9) ) (= n2 (- n1 1))) ; if not on left border, one left
@@ -25,24 +25,25 @@
 
 
 ;;ROBOT Specification
+
 (defconstant robotPredicates
        (alw 
            (<-> (-P- robotMoving)
-            (-E- p1 *ncells* (-E- p2 *ncells* (&& (/= p1 p2) (-P- robotIn p1) (next (-P- robotIn p2))));the robot is moving if current position and future position are different
+            (-E- p1 *ncells* (-E- p2 *ncells* (&& (/= p1 p2) (-P- robotIn p1) (next (-P- robotIn p2))));The robot is moving iff current position and future position are different
            ))
            
         )
 )
 
-(defvar robotAlwaysSomewhere
+(defvar robotAlwaysSomewhere ;Asserts that there always exists an unique position for the robot
     (alw 
         (-E- a *ncells* 
             (&&
-                (-P- robotIn a) ;;exists a valid position for the robot
+                (-P- robotIn a) ;Exists a valid position for the robot
                 (!!(-E- b *ncells* 
                     (&& 
                         (/= b a)
-                        (-P- robotIn b) ;;this position is unique
+                        (-P- robotIn b) ;This position is unique
                     )))
             )
         )
@@ -52,10 +53,10 @@
 (defconstant realisticRobotMovement
     (-A- a *ncells* 
         (alw
-            (-> (yesterday (-P- robotIn a)) (-E- b *ncells* 
+            (-> (yesterday (-P- robotIn a)) (-E- b *ncells* ;If robot was in a cell a last time instant
                 (&&
                   (-P- robotIn b)
-                    (adjacent a b) ;;robot can be in a position b only if it was in an adjacent cell a
+                    (adjacent a b) ;robot can be in a position b only if it is adjacent to cell a
                 ))
             )
             
@@ -63,42 +64,48 @@
     )
 )
 
-(defconstant robotState
+(defconstant robotState ;Defines the automaton modeling the robot
     (alw (&&
             (!! (&& (-P- robotToL9) (-P- robotToL3)))
             (!! (&& (-P- robotWorking) (-P- robotToL9)))
             (!! (&& (-P- robotWorking) (-P- robotToL3)))
-            (|| (-P- robotWorking) (-P- robotToL9) (-P- robotToL3)) ;exactly one of those proposition is true at each time
+            (|| (-P- robotWorking) (-P- robotToL9) (-P- robotToL3)) ;exactly one of those propositions is true at each time
             
-            (-> (&& (-P- robotToL9) (next (-P- robotIn 9)))  (next (-P- robotWorking))) ;If next time I reach 9, start working
-            (-> (&& (-P- robotToL9) (next(!! (-P- robotIn 9)))) (next (-P- robotToL9))) ;If next time I don't reach 9, keep going
+            (-> (&& (-P- robotToL9) (next (-P- robotIn 9)))  (next (-P- robotWorking))) ;If next time robot reaches 9, start working
+            (-> (&& (-P- robotToL9) (next(!! (-P- robotIn 9)))) (next (-P- robotToL9))) ;If next time robot doesn't reach 9, keep going
             
-            (-> (&& (-P- robotToL3) (next (-P- robotIn 3)))  (next (-P- robotWorking))) ;If next time I reach 3, start working
-            (-> (&& (-P- robotToL3) (next(!! (-P- robotIn 3)))) (next (-P- robotToL3))) ;If next time I don't reach 3, keep going
+            (-> (&& (-P- robotToL3) (next (-P- robotIn 3)))  (next (-P- robotWorking))) ;If next time robot reaches 3, start working
+            (-> (&& (-P- robotToL3) (next(!! (-P- robotIn 3)))) (next (-P- robotToL3))) ;If next time robot doesn't reach 3, keep going
             
-            (-> (&& (-P- robotWorking) (next (-P- robotWorking)) (-P- robotIn 3))  (next (-P- robotIn 3))) ; If I am working in 3 and will work next time, stay in 3
-            (-> (&& (-P- robotWorking) (next (-P- robotToL9)) (-P- robotIn 3))  (next (!!(-P- robotIn 3)))); If I am working in 3 and change state, next time go away from 3
-            (-> (&& (-P- robotWorking) (-P- robotIn 3))  (!! (next (-P- robotToL3)))) ; If I am working in 3, I can only keep working or go to robotToL9 state
+            (-> (&& (-P- robotWorking) (next (-P- robotWorking)) (-P- robotIn 3))  (next (-P- robotIn 3))) ; If robot is working in 3 and will work next time, stay in 3
+            (-> (&& (-P- robotWorking) (next (-P- robotToL9)) (-P- robotIn 3))  (next (!!(-P- robotIn 3)))); If robot is working in 3 and change state, next time instant go away from 3
+            (-> (&& (-P- robotWorking) (-P- robotIn 3))  (!! (next (-P- robotToL3)))) ; If robot is working in 3 it can only keep working or go to robotToL9 state
             
-            (-> (&& (-P- robotWorking) (next (-P- robotWorking)) (-P- robotIn 9))  (next (-P- robotIn 9))) ; If I am working in 9 and will work next time, stay in 9
-            (-> (&& (-P- robotWorking) (next (-P- robotToL3)) (-P- robotIn 9))  (next (!!(-P- robotIn 9)))); If I am working in 9 and change state, next time go away from 9
-            (-> (&& (-P- robotWorking) (-P- robotIn 9))  (!! (next (-P- robotToL9)))) ; If I am working in 9, I can only keep working or go to robotToL3 state
+            (-> (&& (-P- robotWorking) (next (-P- robotWorking)) (-P- robotIn 9))  (next (-P- robotIn 9))) ; If robot is working in 9 and will work next time, stay in 9
+            (-> (&& (-P- robotWorking) (next (-P- robotToL3)) (-P- robotIn 9))  (next (!!(-P- robotIn 9)))); If robot is working in 9 and change state, next time go away from 9
+            (-> (&& (-P- robotWorking) (-P- robotIn 9))  (!! (next (-P- robotToL9)))) ; If robot is working in 9 it can only keep working or go to robotToL3 state
             
 ))) 
 
 
-(defconstant robotController
+(defconstant robotController ;Defines a controller for the robot
     (alw (&&
-            (!! (&& (-P- collision) (-P- robotMoving)));This means that the controller stops the robot if an operator is in the same cell
+            (!! (&& (-P- collision) (-P- robotMoving)));The controller stops the robot if an operator is in the same cell
             
-            ;Entering an area where an operator is going is not acceptable(assuming next operator position is known)
+            ;Entering an area where an operator is going is not acceptable
             (-A- p *ncells* (-> (&& (next (-P- operatorIn p)) (-P- robotMoving) )  (!!(next (-P- robotIn p))) ))
     
             ;If robot is going to 9 and it is in an adjacent cell to it, either it goes to destination or it is blocked by operator and stops, no useless deviation
-            (-> (-P- robotToL9) (-> (-E- p *ncells* (&& (-P- robotIn p) (adjacent p 9))) #|implies|# (|| (next (-P- robotIn 9)) (&& (next(-P- operatorIn 9)) (!! (-P- robotMoving)) ) )  ))
+            (-> 
+                (-P- robotToL9) 
+                (-> (-E- p *ncells* (&& (-P- robotIn p) (adjacent p 9))) #|implies|# (|| (next (-P- robotIn 9)) (&& (next(-P- operatorIn 9)) (!! (-P- robotMoving)) ) )  )
+            )
             
             ;If robot is going to 3 and it is in an adjacent cell to it, either it goes to destination or it is blocked by operator and stops, no useless deviation
-            (-> (-P- robotToL3) (-> (-E- p *ncells* (&& (-P- robotIn p) (adjacent p 3))) #|implies|# (|| (next (-P- robotIn 3)) (&& (next(-P- operatorIn 3)) (!! (-P- robotMoving)) ) )  ))
+            (-> 
+                (-P- robotToL3) 
+                (-> (-E- p *ncells* (&& (-P- robotIn p) (adjacent p 3))) #|implies|# (|| (next (-P- robotIn 3)) (&& (next(-P- operatorIn 3)) (!! (-P- robotMoving)) ) )  )
+            )
 
 )))
 
@@ -113,7 +120,7 @@
         )
 )
 
-(defvar operatorAlwaysSomewhere
+(defvar operatorAlwaysSomewhere ;Asserts that there always exists an unique position for the operator
     (alw 
         (-E- a *ncells* 
             (&&
@@ -126,7 +133,6 @@
             )
         )
 ))
-    
     
 (defconstant realisticOperatorMovement
     (-A- a *ncells* 
@@ -142,26 +148,26 @@
     )
 )
 
-(defconstant operatorState ;10 8
+(defconstant operatorState ;Defines the automaton modeling the operator
     (alw (&&
             (!! (&& (-P- operatorToL10) (-P- operatorToL8)))
             (!! (&& (-P- operatorWorking) (-P- operatorToL10)))
             (!! (&& (-P- operatorWorking) (-P- operatorToL8)))
-            (|| (-P- operatorWorking) (-P- operatorToL10) (-P- operatorToL8)) ;exactly one of those proposition is true at each time
+            (|| (-P- operatorWorking) (-P- operatorToL10) (-P- operatorToL8)) ;exactly one of those propositions is true at each time
             
-            (-> (&& (-P- operatorToL10) (next (-P- operatorIn 10)))  (next (-P- operatorWorking))) ;If next time I reach 10, start working
-            (-> (&& (-P- operatorToL10) (next(!! (-P- operatorIn 10)))) (next (-P- operatorToL10))) ;If next time I don't reach 10, keep going
+            (-> (&& (-P- operatorToL10) (next (-P- operatorIn 10)))  (next (-P- operatorWorking))) ;If next time operator reaches 10, start working
+            (-> (&& (-P- operatorToL10) (next(!! (-P- operatorIn 10)))) (next (-P- operatorToL10))) ;If next time operator doesn't reach 10, keep going
             
-            (-> (&& (-P- operatorToL8) (next (-P- operatorIn 8)))  (next (-P- operatorWorking))) ;If next time I reach 8, start working
-            (-> (&& (-P- operatorToL8) (next(!! (-P- operatorIn 8)))) (next (-P- operatorToL8))) ;If next time I don't reach 8, keep going
+            (-> (&& (-P- operatorToL8) (next (-P- operatorIn 8)))  (next (-P- operatorWorking))) ;If next time operator reaches 8, start working
+            (-> (&& (-P- operatorToL8) (next(!! (-P- operatorIn 8)))) (next (-P- operatorToL8))) ;If next time operator doesn't reach 8, keep going
             
-            (-> (&& (-P- operatorWorking) (next (-P- operatorWorking)) (-P- operatorIn 8))  (next (-P- operatorIn 8))) ; If I am working in 8 and will work next time, stay in 8
-            (-> (&& (-P- operatorWorking) (next (-P- operatorToL10)) (-P- operatorIn 8))  (next (!!(-P- operatorIn 8)))); If I am working in 8 and change state, next time go away from 8
-            (-> (&& (-P- operatorWorking) (-P- operatorIn 8))  (!! (next (-P- operatorToL8)))) ; If I am working in 8, I can only keep working or go to operatorToL10 state
+            (-> (&& (-P- operatorWorking) (next (-P- operatorWorking)) (-P- operatorIn 8))  (next (-P- operatorIn 8))) ; If operator is working in 8 and will work next time, stay in 8
+            (-> (&& (-P- operatorWorking) (next (-P- operatorToL10)) (-P- operatorIn 8))  (next (!!(-P- operatorIn 8)))); If operator is working in 8 and change state, next time go away from 8
+            (-> (&& (-P- operatorWorking) (-P- operatorIn 8))  (!! (next (-P- operatorToL8)))) ; If operator is working in 8 it can only keep working or go to operatorToL10 state
             
-            (-> (&& (-P- operatorWorking) (next (-P- operatorWorking)) (-P- operatorIn 10))  (next (-P- operatorIn 10))) ; If I am working in 10 and will work next time, stay in 10
-            (-> (&& (-P- operatorWorking) (next (-P- operatorToL8)) (-P- operatorIn 10))  (next (!!(-P- operatorIn 10)))); If I am working in 10 and change state, next time go away from 10
-            (-> (&& (-P- operatorWorking) (-P- operatorIn 10))  (!! (next (-P- operatorToL10)))) ; If I am working in 10, I can only keep working or go to operatorToL8 state
+            (-> (&& (-P- operatorWorking) (next (-P- operatorWorking)) (-P- operatorIn 10))  (next (-P- operatorIn 10))) ; If operator is working in 10 and will work next time, stay in 10
+            (-> (&& (-P- operatorWorking) (next (-P- operatorToL8)) (-P- operatorIn 10))  (next (!!(-P- operatorIn 10)))); If operator is working in 10 and change state, next time go away from 10
+            (-> (&& (-P- operatorWorking) (-P- operatorIn 10))  (!! (next (-P- operatorToL10)))) ; If operator is working in 10 it can only keep working or go to operatorToL8 state
             
 ))) 
 
@@ -190,7 +196,7 @@
             ;(-P- robotToL9)
             ;(next(-P- robotIn 10))
             ;(next(-P- operatorIn 10))
-            (next (next (-P- robotIn 3)))
+            ;(next (next (-P- robotIn 3)))
             ))
     
 ;Objective property
@@ -199,19 +205,21 @@
         (<-> (-P- collision) (-E- p *ncells* (&& (-P- operatorIn p)(-P- robotIn p) )))
 ))
 
-(defconstant collisionEnforcer
+(defconstant collisionEnforcer ;Defines the property that must make the model unsatisfiable
     ;(somF (&& (-P- collision) (-P- robotMoving))) ;Moving collision
-    ;(somF  (-P- collision) ) ;Simple collision
-    (somF (|| (&& (-P- collision) (-P- robotMoving)) (&& (next (-P- collision)) (-P- robotMoving))  )) ;Moving collision and converging to operator position 
+    (somF (|| 
+            (&& (-P- collision) (-P- robotMoving))
+            (&& (next (-P- collision)) (-P- robotMoving))  
+        )) ;Moving collision or robot converging to operator position 
 )
 
-(defconstant niceSimulation
+(defconstant niceSimulation ;Defines axioms that force the simulation to show a couple of iterations instead of random paths
     (&&
-        (next (!! (-P- robotIn 3))) ;Next time instant go to 9
-        (next (somF (&& (-P- robotWorking) (-P- robotIn 3)))) ;Counting from next time instant, robot does at least a full loop
+        (next (!! (-P- robotIn 3))) ;Next time instant go to robotToL9 state
+        (next (somF (&& (-P- robotWorking) (-P- robotIn 3)))) ;Counting from next time instant, the robot does at least a full loop
         
-        (next (!! (-P- operatorIn 10))) ;Next time instant go to 8
-        (next (somF (&& (-P- operatorWorking) (-P- operatorIn 10)))) ;Counting from next time instant, operator does at least a full loop
+        (next (!! (-P- operatorIn 10))) ;Next time instant go to operatorToL8 state
+        (next (somF (&& (-P- operatorWorking) (-P- operatorIn 10)))) ;Counting from next time instant, the operator does at least a full loop
 
 ))
 
@@ -237,7 +245,7 @@
                     
                     ;Collision definition
                     collision
-                    collisionEnforcer
+                    ;collisionEnforcer
                     
                     ;Simulation guiding
                     niceSimulation
